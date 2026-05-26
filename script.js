021d63990b87a40774db217e0a635a34fefd647b
Felipe <fgutierrrez798@gmail.com>
2026-05-26 10:12:22 -0400

    prueba commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Creamos la constante de botón
+const boton = document.getElementById("submit");
+
+//Agregamos el evento Click al boton creado anteriormente
+boton.addEventListener("click", () => {
+    //Mandamos un mensaje por consola para comprobar que funcione.
+    console.log("Click")
+})
